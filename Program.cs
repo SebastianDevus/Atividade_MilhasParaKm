9efd30528dd1d693017913c553614caf3b3c5216
SebastianDevus <sebastianjogosextra@gmail.com>
2022-03-19 12:30:23 -0300

    Atividade Realizada

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿const double constanteConversao = 1.609;
+double milhas, quilometros;
+
+Console.Write("Por favor, insira a distância em milhas: ");
+milhas = Convert.ToDouble(Console.ReadLine()!);
+
+quilometros = milhas * constanteConversao;
+
+Console.WriteLine($"\nO equivalente em quilômetros é {quilometros} Km");
